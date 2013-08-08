39cc6b3e437430a2b21dcd99fb962d72ac1837fb
Orange <nicholas@compasslabs.com>
2013-08-07 18:08:58 -0700

    .

--- router.js
+++ router.js
@@ -6,17 +6,36 @@ function route(parsedUrl, request, response) {
 	var pathname = parsedUrl.pathname,
 		query = parsedUrl.query;
 
-	switch(pathname) {
-		case "/":
-			// Explicitly ignore the call for favicon
-			displayRoot(request, response);
-			break;
-		case "/getSubjects":
-			getSubjects(query, request, response);
-			break;
-		default:
-			display404(pathname, request, response);
-			break;
+	var origin = (request.headers.origin || "*");
+
+	if( request.method.toUpperCase() === "OPTIONS" ) {
+		response.writeHead(
+			"204",
+			"No Content",
+			{
+				"access-control-allow-origin": origin,
+                "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+                "access-control-allow-headers": "content-type, accept",
+                "access-control-max-age": 10, // Seconds.
+                "content-length": 0
+			}
+		);
+
+		return( response.end() );
+	}
+	else {
+		switch(pathname) {
+			case "/":
+				// Explicitly ignore the call for favicon
+				displayRoot(request, response);
+				break;
+			case "/getSubjects":
+				getSubjects(query, request, response);
+				break;
+			default:
+				display404(pathname, request, response);
+				break;
+		}
 	}
 }
 
@@ -37,13 +56,12 @@ function getSubjects(query, request, response) {
 		database: "askitdb",
 	});
 
+	var origin = (request.headers.origin || "*");
+
 	connection.query("SELECT DISTINCT subject FROM topics WHERE level='Secondary 1'", function(err, rows, fields) {
 		if (err) throw err;
 
-		// response.writeHead(200, {"Content-Type": "text/plain"});
-		// response.write(JSON.stringify(rows));
-		response.writeHead(200, {"Content-Type": "application/json"});
-		// response.write();
+		response.writeHead(200, {"access-control-allow-origin": origin, "Content-Type": "application/json"});
 		response.end(JSON.stringify(rows));
 	});
 }
